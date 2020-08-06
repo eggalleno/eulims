a6118289a4866f5a40364b29edc92fcae0d92ff0
Bergel <b.cutara@gmail.com>
2020-08-06 08:14:01 +0800

    undoing 2 lines of update from last patch of customer/firm accomplishment report
    tsaadaa

--- frontend/modules/reports/modules/lab/controllers/AccomplishmentController.php
+++ frontend/modules/reports/modules/lab/controllers/AccomplishmentController.php
@@ -55,10 +55,9 @@ class AccomplishmentController extends \yii\web\Controller
 			'totalrequests' => 'count(request_id)',
 			'total'=>'SUM(total)',
 			'request_datetime',
-            'yearmonth'=>'DATE_FORMAT(request_datetime, "%Y-%m")',
 		])
 		->where('rstl_id =:rstlId AND status_id > :statusId AND lab_id = :labId AND DATE_FORMAT(`request_datetime`, "%Y") = :year AND request_ref_num != ""', [':rstlId'=>$rstlId,':statusId'=>0,':labId'=>$labId,':year'=>$year])
-		->groupBy(['request_datetime'])
+		->groupBy(['DATE_FORMAT(request_datetime, "%Y-%m")'])
 		->orderBy('request_datetime ASC');
 
 
